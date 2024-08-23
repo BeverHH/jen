a9e96f508edf4ff62c6d92f26966418561d0b616
Jun Hong <rjseka223@gmail.com>
2024-08-23 11:45:14 +0900

    test

---
+++ src/main/java/com/example/jen1/Jen1Application.java
@@ -0,0 +1,13 @@
+package com.example.jen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jen1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jen1/Jen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
